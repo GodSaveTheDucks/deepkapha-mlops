7b63ef70f6912a9c707fee87e8bcedaebef93c1f
GodSaveTheDucks <suhas@claire.ai>
2021-04-27 19:58:36 +0530

    updated

--- get_data.py
+++ get_data.py
@@ -8,15 +8,15 @@ class GetDataController(object):
         self.urls = {
             'wind_farm_data': 'https://www.thecrownestate.co.uk/api/energy-map/wind-farm-data',}
     
-    def generate_file_name(self):
-        return str(datetime.datetime.utcnow())
-        
+    
     def execute(self,data_dir='data'):
         for filename, endpoint_url in self.urls.items():
             r = requests.get(endpoint_url)
-            filename = ''.join([filename,"-",self.generate_file_name()])
+            return r
             
 
 def handle(event,context):
     controller = GetDataController()
-    controller.execute()
+    return {
+        "data" : json.dumps(controller.execute())
+    }
